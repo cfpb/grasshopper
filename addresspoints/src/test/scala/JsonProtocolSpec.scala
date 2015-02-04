71d28181597fcbf659666741cf2142bc85b4a6aa
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-02-04 09:29:58 -0500

    Add AddressInput serialization test

--- addresspoints/src/test/scala/JsonProtocolSpec.scala
+++ addresspoints/src/test/scala/JsonProtocolSpec.scala
@@ -9,9 +9,12 @@ class JsonProtocolSpec extends FlatSpec with MustMatchers with JsonProtocol {
   "Status" should "convert to and from JSON" in {
     val date = Calendar.getInstance().getTime().toString
     val status = Status("OK", date)
-    println(status.toJson)
     status.toJson.convertTo[Status] mustBe status
   }
 
+  "AddressInput" should "convert to and from JSON" in {
+    val addressInput = AddressInput(1, "1311 30th St NW Washington DC 20007")
+    addressInput.toJson.convertTo[AddressInput] mustBe addressInput
+  }
 }
 
