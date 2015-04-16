f57a44386189d6dfbcf403428b8e8edba82e3c06
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-04-16 11:59:17 -0400

    Add hostname to Status endpoint

--- addresspoints/src/main/scala/addresspoints/protocol/JsonProtocol.scala
+++ addresspoints/src/main/scala/addresspoints/protocol/JsonProtocol.scala
@@ -4,6 +4,6 @@ import addresspoints.model.{ AddressInput, Status }
 import spray.json.DefaultJsonProtocol
 
 trait JsonProtocol extends DefaultJsonProtocol {
-  implicit val statusFormat = jsonFormat2(Status.apply)
+  implicit val statusFormat = jsonFormat3(Status.apply)
   implicit val addressInputFormat = jsonFormat1(AddressInput.apply)
 }
