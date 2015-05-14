f27674210faf835fef7be40ed0e8f7e1005fd76f
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-05-13 21:32:15 -0400

    Add addresspoints client

---
+++ client/src/it/scala/client/addresspoints/AddressPointsClientSpec.scala
@@ -0,0 +1,49 @@
+package grasshopper.client.addresspoints
+
+import grasshopper.client.addresspoints.model.AddressPointsStatus
+import org.scalatest._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class AddressPointsJsonProtocolSpec extends FlatSpec with MustMatchers {
+
+ "A request to /status" must "return a status object" in {
+    val maybeStatus: Either[String, AddressPointsStatus] = Await.result(AddressPointsClient.status, 10.seconds)
+    maybeStatus match {
+      case Right(s) =>
+        s.status mustBe "OK"
+      case Left(_) =>
+        fail("The call to /status failed")
+    }
+  }
+ 
+  it must "geocode an address string" in {
+    val maybeAddress = Await.result(AddressPointsClient.geocode("108+S+Main+St+Bentonville+AR+72712"), 10.seconds)
+    maybeAddress match {
+      case Right(features) =>
+        features.size mustBe 1
+        val f = features(0)
+        val address = f.values.getOrElse("address", "")
+        address mustBe "108 S Main St Bentonville AR 72712"
+      case Left(_) =>
+        fail("The call to /geocode failed")
+    }
+  }
+
+  it must "offer candidate address results with suggest parameter in request" in {
+ val maybeAddress = Await.result(AddressPointsClient.geocode("president?suggest=5"), 10.seconds)
+    maybeAddress match {
+      case Right(features) =>
+        features.size mustBe 5
+        val f = features(0)
+        val address = f.values.getOrElse("address", "")
+        address mustBe "701 President St Arkansas City AR 71630"
+      case Left(_) =>
+        fail("The call to /geocode failed")
+    } 
+  
+  }
+
+
+}
+
--- client/src/main/scala/client/addresspoints/AddressPointsClient.scala
+++ client/src/main/scala/client/addresspoints/AddressPointsClient.scala
@@ -1,31 +1,34 @@
-package grasshopper.client.parser
+package grasshopper.client.addresspoints
 
-import java.io.IOException
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.stream.ActorFlowMaterializer
 import com.typesafe.config.ConfigFactory
-import grasshopper.client.ServiceClient
-import grasshopper.client.parser.model.{ ParserStatus, ParsedAddress }
-import grasshopper.client.parser.protocol.ParserJsonProtocol
-import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.Properties
+import grasshopper.client.ServiceClient
+import grasshopper.client.addresspoints.model.AddressPointsStatus
+import grasshopper.client.addresspoints.protocol.AddressPointsJsonProtocol
+import scala.concurrent.{ ExecutionContext, Future }
+import java.io.IOException
+import feature._
+import io.geojson.FeatureJsonProtocol._
 
-object AddressParserClient extends ServiceClient with ParserJsonProtocol {
-  override implicit val system: ActorSystem = ActorSystem("grasshopper-client-parser")
+object AddressPointsClient extends ServiceClient with AddressPointsJsonProtocol {
+
+  override implicit val system: ActorSystem = ActorSystem("grasshopper-client-addresspoints")
   override implicit val materializer: ActorFlowMaterializer = ActorFlowMaterializer()
   override val config = ConfigFactory.load()
 
-  lazy val host = Properties.envOrElse("GRASSHOPPER_PARSER_HOST", config.getString("parser.host"))
-  lazy val port = Properties.envOrElse("GRASSHOPPER_PARSER_PORT", config.getString("parser.port"))
+  lazy val host = Properties.envOrElse("GRASSHOPPER_ADDRESSPOINTS_HOST", config.getString("addresspoints.host"))
+  lazy val port = Properties.envOrElse("GRASSHOPPER_ADDRESSPOINTS_PORT", config.getString("addresspoints.port"))
 
-  def status(): Future[Either[String, ParserStatus]] = {
+  def status(): Future[Either[String, AddressPointsStatus]] = {
     implicit val ec: ExecutionContext = system.dispatcher
     sendGetRequest(host, port.toInt, "/status").flatMap { response =>
       response.status match {
-        case OK => Unmarshal(response.entity).to[ParserStatus].map(Right(_))
+        case OK => Unmarshal(response.entity).to[AddressPointsStatus].map(Right(_))
         case BadRequest => Future.successful(Left("Bad Request"))
         case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
           val error = s"Request failed with status code ${response.status} and entity ${entity}}"
@@ -35,19 +38,19 @@ object AddressParserClient extends ServiceClient with ParserJsonProtocol {
     }
   }
 
-  def parse(address: String): Future[Either[String, ParsedAddress]] = {
+  def geocode(address: String): Future[Either[String, List[Feature]]] = {
     implicit val ec: ExecutionContext = system.dispatcher
-    sendGetRequest(host, port.toInt, s"/parse?address=${address}").flatMap { response =>
+    sendGetRequest(host, port.toInt, s"/addresses/points/${address}").flatMap { response =>
       response.status match {
-        case OK => Unmarshal(response.entity).to[ParsedAddress].map(Right(_))
+        case OK => Unmarshal(response.entity).to[List[Feature]].map(Right(_))
         case BadRequest => Future.successful(Left("Bad Request"))
         case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
           val error = s"Request failed with status code ${response.status} and entity ${entity}}"
           Future.failed(new IOException(error))
         }
       }
-
     }
+
   }
 
 }
