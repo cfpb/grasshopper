f5598e0977369dc04e9364e6988c3cb8cba3f246
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-05-27 11:12:23 -0400

    Update test

--- geocoder/src/test/scala/grasshopper/geocoder/protocol/GrasshopperJsonProtocolSpec.scala
+++ geocoder/src/test/scala/grasshopper/geocoder/protocol/GrasshopperJsonProtocolSpec.scala
@@ -52,6 +52,7 @@ class GrasshopperJsonProtocolSpec extends FlatSpec with MustMatchers with Grassh
     val addressPointsServiceStr =
       """
            {
+             "status": "OK",
              "features": [
                {
                  "type": "Feature",
@@ -77,81 +78,67 @@ class GrasshopperJsonProtocolSpec extends FlatSpec with MustMatchers with Grassh
     addressPointService.features(0).get("load_date").getOrElse(0) mustBe 1426878178730L
   }
 
-  //  "A geocode result" must "serialize from JSON" in {
-  //    val geocodeResultStr = """
-  //       {
-  //         "status": "OK",
-  //         "query": {
-  //           "input": "1311 30th St Washington DC 20007",
-  //           "parts": {
-  //             "AddressNumber": "1311",
-  //             "PlaceName": "washington",
-  //             "StateName": "dc",
-  //             "StreetName": "30th",
-  //             "StreetNamePostType": "st",
-  //             "ZipCode": "20007"
-  //           }
-  //         },
-  //         "features": [
-  //         {
-  //           "service": "census",
-  //           "data": {
-  //              "type": "Feature",
-  //              "geometry": {
-  //                "type": "Point",
-  //                "coordinates": [
-  //                  -77.05908853531027,
-  //                  38.90721451814751,
-  //                  0
-  //                ]
-  //              },
-  //              "properties": {
-  //                "RFROMHN": "1301",
-  //                "RTOHN": "1323",
-  //                "ZIPL": "20007",
-  //                "FULLNAME": "30th St NW",
-  //                "LFROMHN": "1300",
-  //                "LTOHN": "1318",
-  //                "ZIPR": "20007",
-  //                "STATE": "DC"
-  //              }
-  //           }
-  //         },
-  //         {
-  //           "service": "addresspoints",
-  //           "data": {
-  //             "type": "Feature",
-  //             "geometry": {
-  //               "type": "Point",
-  //               "coordinates": [
-  //                 -77.05908853531027,
-  //                 38.90721451814751,
-  //                 0
-  //               ]
-  //             },
-  //             "properties": {
-  //               "address": "1311 30th st nw washington dc 20007",
-  //               "alt_address": "",
-  //               "load_date": 1426878185988
-  //             }
-  //           }
-  //         }
-  //         ]
-  //       }
-  //    """
-  //
-  //    val geocodeResult = geocodeResultStr.parseJson.convertTo[GeocodeResult]
-  //    geocodeResult.status mustBe "OK"
-  //    geocodeResult.query.parts.AddressNumber mustBe "1311"
-  //    geocodeResult.query.parts.PlaceName mustBe "washington"
-  //    geocodeResult.query.parts.StateName mustBe "dc"
-  //    geocodeResult.query.parts.StreetName mustBe "30th"
-  //    geocodeResult.query.parts.StreetNamePostType mustBe "st"
-  //    geocodeResult.query.parts.ZipCode mustBe "20007"
-  //    geocodeResult.features.size mustBe 2
-  //    geocodeResult.features(0).service mustBe "census"
-  //    geocodeResult.features(1).service mustBe "addresspoints"
-  //    //geocodeResult.features(0).data.geometry mustBe geocodeResult.features(1).data.geometry
-  //  }
+  "A geocode result" must "serialize from JSON" in {
+    val geocodeResultStr = """
+        {
+          "status": "OK",
+          "query": {
+            "input": "200 President St Arkansas City AR 71630",
+            "parts": {
+              "StreetName": "President",
+              "StreetNamePostType": "St",
+              "StateName": "AR",
+              "PlaceName": "City",
+              "ZipCode": "71630",
+              "AddressNumber": "200"
+            }
+          },
+          "addressPointsService": {
+            "status": "OK",
+            "features": [{
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [-91.19978780015629, 33.608091616155995, 0.0]
+              },
+              "properties": {
+                "address": "200 President St Arkansas City AR 71630",
+                "alt_address": "",
+                "load_date": 1426878185988
+              }
+            }]
+          },
+          "censusService": {
+            "status": "OK",
+            "features": [{
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [-91.19960153268617, 33.60763673811005, 0.0]
+              },
+              "properties": {
+                "RFROMHN": "100",
+                "RTOHN": "498",
+                "ZIPL": "",
+                "FULLNAME": "President St",
+                "LFROMHN": "",
+                "LTOHN": "",
+                "ZIPR": "71630",
+                "STATE": "AR"
+              }
+            }]
+          }
+        }
+      """
+
+    val geocodeResult = geocodeResultStr.parseJson.convertTo[GeocodeResult]
+    geocodeResult.status mustBe "OK"
+    geocodeResult.query.parts.AddressNumber mustBe "200"
+    geocodeResult.query.parts.StateName mustBe "AR"
+    geocodeResult.query.parts.StreetName mustBe "President"
+    geocodeResult.query.parts.StreetNamePostType mustBe "St"
+    geocodeResult.query.parts.ZipCode mustBe "71630"
+
+  }
 
 }
