5b488e7c7aa4afe0e9ed173fc7e979c7e02a8b57
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-06-25 12:06:17 -0400

    Add integration tests for geocode flows

---
+++ geocoder/src/it/scala/grasshopper/geocoder/api/GeocodeFlowsSpec.scala
@@ -0,0 +1,107 @@
+package grasshopper.geocoder.api
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import grasshopper.client.census.model.ParsedInputAddress
+import grasshopper.client.parser.model.{AddressPart, ParsedAddress}
+import grasshopper.geocoder.model.ParsedOutputBatchAddress
+import org.scalatest.{FlatSpec, MustMatchers}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class GeocodeFlowsSpec extends FlatSpec with MustMatchers {
+
+  implicit val system = ActorSystem("sys")
+  implicit val mat = ActorMaterializer()
+  implicit val ec = system.dispatcher
+
+  "GeocodeFlows" must "parse a list of addresses" in {
+    val addresses =
+      List(
+        "1311 30th St NW Washington DC 20007",
+        "3146 M St NW Washington DC 20007",
+        "198 President St Arkansas City AR 71630",
+        "1 Main St City ST 00001"
+      ).toIterator
+
+    val source = Source(() => addresses)
+    val future = source.via(GeocodeFlows.parseFlow).grouped(4).runWith(Sink.head)
+    val result = Await.result(future, 2.seconds)
+    result.size mustBe 4
+    result.head.input mustBe "1311 30th St NW Washington DC 20007"
+    result.head.parts.addressNumber mustBe "1311"
+    result.head.parts.city mustBe "Washington"
+    result.head.parts.state mustBe "DC"
+    result.head.parts.zip mustBe "20007"
+
+    result.tail.head.input mustBe "3146 M St NW Washington DC 20007"
+    result.tail.head.parts.addressNumber mustBe "3146"
+    result.tail.head.parts.city mustBe "Washington"
+    result.tail.head.parts.state mustBe "DC"
+    result.tail.head.parts.zip mustBe "20007"
+  }
+
+  it must "transform parsed addresses into batch parsed addresses" in {
+    val inputParsedList = List(
+      ParsedAddress("1311 30th St NW Washington DC 20007",
+        AddressPart("1311","Washington","DC","30th St NW","20007")),
+      ParsedAddress("3146 M St NW Washington DC 20007",
+        AddressPart("3146","Washington","DC","M St NW","20007")),
+      ParsedAddress("198 President St Arkansas City AR 71630",
+        AddressPart("198","Arkansas City","AR","President St","71630")),
+      ParsedAddress("1 Main St City ST 00001",AddressPart("1","St City","ST","Main","00001")))
+
+    val source = Source(() => inputParsedList.toIterator)
+    val future = source.via(GeocodeFlows.parsedInputAddressFlow).grouped(4).runWith(Sink.head)
+    val result = Await.result(future, 2.seconds)
+    result.size mustBe 4
+    result.head.input mustBe "1311 30th St NW Washington DC 20007"
+    result.head.parsed mustBe ParsedInputAddress(1311, "30th St NW", 20007, "DC")
+
+    result.tail.head.input mustBe "3146 M St NW Washington DC 20007"
+    result.tail.head.parsed mustBe ParsedInputAddress(3146, "M St NW", 20007, "DC")
+  }
+
+  it must "geocode batch parsed addresses into TIGER line results" in {
+    val parsedBatchList =
+      List(
+        ParsedOutputBatchAddress("1311 30th St NW Washington DC 20007",
+          ParsedInputAddress(1311, "30th St NW", 20007, "DC")),
+        ParsedOutputBatchAddress("3146 M St NW Washington DC 20007",
+          ParsedInputAddress(3146, "M St NW",20007, "DC")),
+        ParsedOutputBatchAddress("198 President St Arkansas City AR 71630",
+          ParsedInputAddress(198, "President St", 71630, "AR")),
+        ParsedOutputBatchAddress("1 Main St City ST 00001",
+          ParsedInputAddress(1, "Main ST", 1,"ST")))
+
+    val source = Source(() => parsedBatchList.toIterator)
+    val future = source.via(GeocodeFlows.censusFlow).grouped(4).runWith(Sink.head)
+    val result = Await.result(future, 2.seconds)
+    result.size mustBe 4
+    result.head.input mustBe "1311 30th St NW Washington DC 20007"
+    result.head.latitude mustBe 38.907211343944184
+    result.head.longitude mustBe -77.05928851011977
+  }
+
+  it must "geocode addresses into points" in {
+    val addresses =
+      List(
+        "1311 30th St NW Washington DC 20007",
+        "3146 M St NW Washington DC 20007",
+        "198 President St Arkansas City AR 71630",
+        "1 Main St City ST 00001"
+      ).toIterator
+
+    val source = Source(() => addresses)
+    val future = source.via(GeocodeFlows.addressPointsFlow).grouped(4).runWith(Sink.head)
+    val result = Await.result(future, 2.seconds)
+    println(result)
+    result.size mustBe 4
+    result.tail.tail.head.input mustBe "198 President St Arkansas City AR 71630"
+    result.tail.tail.head.latitude mustBe 33.60824683723317
+    result.tail.tail.head.longitude mustBe -91.19986550300061
+
+  }
+}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,25 +7,27 @@ object Dependencies {
     "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases"
   )
 
-  val akkaActor        = "com.typesafe.akka"          %% "akka-actor"                           % Version.akka
-  val akkaStreams      = "com.typesafe.akka"          %% "akka-stream-experimental"             % Version.akkaStreams
-  val akkaHttpCore     = "com.typesafe.akka"          %% "akka-http-core-experimental"          % Version.akkaStreams
-  val akkaHttp         = "com.typesafe.akka"          %% "akka-http-experimental"               % Version.akkaStreams
-  val akkaHttpJson     = "com.typesafe.akka"          %% "akka-http-spray-json-experimental"    % Version.akkaStreams
-  val akkaHttpTestkit  = "com.typesafe.akka"          %% "akka-http-testkit-experimental"       % Version.akkaStreams % "test"
-  val logback         = "ch.qos.logback"              % "logback-classic"                      % Version.logback
-  val scalaLogging    = "com.typesafe.scala-logging" %% "scala-logging"                        % Version.scalaLogging
+  val akkaActor          = "com.typesafe.akka"          %% "akka-actor"                           % Version.akka
+  val akkaTestKit        = "com.typesafe.akka"          %% "akka-testkit"                         % Version.akka % "test"
+  val akkaStreams        = "com.typesafe.akka"          %% "akka-stream-experimental"             % Version.akkaStreams
+  val akkaStreamsTestkit = "com.typesafe.akka"          %% "akka-stream-testkit-experimental"     % Version.akkaStreams % "test"
+  val akkaHttpCore       = "com.typesafe.akka"          %% "akka-http-core-experimental"          % Version.akkaStreams
+  val akkaHttp           = "com.typesafe.akka"          %% "akka-http-experimental"               % Version.akkaStreams
+  val akkaHttpJson       = "com.typesafe.akka"          %% "akka-http-spray-json-experimental"    % Version.akkaStreams
+  val akkaHttpTestkit    = "com.typesafe.akka"          %% "akka-http-testkit-experimental"       % Version.akkaStreams % "test"
+  val logback            = "ch.qos.logback"              % "logback-classic"                      % Version.logback
+  val scalaLogging       = "com.typesafe.scala-logging" %% "scala-logging"                        % Version.scalaLogging
 
-  val scalaTest        = "org.scalatest"              %% "scalatest"                            % Version.scalaTest   % "it, test"
-  val scalaCheck       = "org.scalacheck"             %% "scalacheck"                           % Version.scalaCheck  % "it, test"
+  val scalaTest          = "org.scalatest"              %% "scalatest"                            % Version.scalaTest   % "it, test"
+  val scalaCheck         = "org.scalacheck"             %% "scalacheck"                           % Version.scalaCheck  % "it, test"
 
-  val es               = "org.elasticsearch"           % "elasticsearch"                        % Version.elasticsearch
+  val es                 = "org.elasticsearch"           % "elasticsearch"                        % Version.elasticsearch
 
-  val scaleGeoJson     = "com.github.jmarin"          %% "scale-geojson"                        % Version.scale
+  val scaleGeoJson       = "com.github.jmarin"          %% "scale-geojson"                        % Version.scale
 
-  val async            = "org.scala-lang.modules"     %% "scala-async"                          % Version.async
+  val async              = "org.scala-lang.modules"     %% "scala-async"                          % Version.async
 
-  val metricsJvm       = "io.dropwizard.metrics"       % "metrics-jvm"                          % Version.metrics
-  val influxDbReporter = "net.alchim31"                % "metrics-influxdb"                     % Version.influxdbReporter
-  val metrics          = "nl.grons"                   %% "metrics-scala"                        % Version.metricsScala excludeAll(ExclusionRule(organization = "com.typesafe.akka"))
+  val metricsJvm         = "io.dropwizard.metrics"       % "metrics-jvm"                          % Version.metrics
+  val influxDbReporter   = "net.alchim31"                % "metrics-influxdb"                     % Version.influxdbReporter
+  val metrics            = "nl.grons"                   %% "metrics-scala"                        % Version.metricsScala excludeAll(ExclusionRule(organization = "com.typesafe.akka"))
 }
