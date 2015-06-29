43b5e17c7376fc2e940be860e3954aa5c832325c
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-06-29 09:36:40 -0400

    Remove unused class

--- geocoder/src/main/scala/grasshopper/geocoder/model/ParsedInputBatchAddress.scala
+++
@@ -1,5 +0,0 @@
-//package grasshopper.geocoder.model
-//
-//import grasshopper.client.census.model.ParsedInputAddress
-//
-//case class ParsedInputBatchAddress(input: String, parsedInputAddress: ParsedInputAddress)
