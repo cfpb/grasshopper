fa7dbb83cb4bd93487075d8aef3bfef8ff2748b5
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-11-02 14:29:06 -0500

    Add batch geocoder integration test

--- geocoder/src/it/scala/grasshopper/geocoder/http/HttpServiceSpec.scala
+++ geocoder/src/it/scala/grasshopper/geocoder/http/HttpServiceSpec.scala
@@ -1,12 +1,17 @@
 package grasshopper.geocoder.http
 
+import java.io.File
 import java.net.URLEncoder
 
 import akka.event.{ NoLogging, LoggingAdapter }
 import akka.http.scaladsl.model.MediaTypes._
+import akka.http.scaladsl.model.Multipart.FormData
 import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.model.Uri
+import akka.http.scaladsl.model.{HttpEntity, Uri}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.stream.io.SynchronousFileSource
+import akka.stream.scaladsl.{Flow, Source, Sink}
+import akka.util.ByteString
 import com.typesafe.config.Config
 import grasshopper.client.parser.model.{ AddressPart, ParsedAddress }
 import grasshopper.elasticsearch.ElasticsearchServer
@@ -59,5 +64,35 @@ class HttpServiceSpec extends FlatSpec with MustMatchers with ScalatestRouteTest
       resp.query mustBe ParsedAddress("3146 M St NW Washington DC 20007",AddressPart("3146","Washington", "DC", "M St NW","20007"))
     }
   }
+
+  it should "perform batch geocoding" in {
+    val file = new File("geocoder/src/test/resources/addresses.csv")
+    val fileSource = SynchronousFileSource(file)
+    val entity = HttpEntity(`application/octet-stream`, file.length(), fileSource)
+    val formData =
+      FormData(
+        Source.single(
+          FormData.BodyPart(
+            "address",
+            entity,
+            Map("filename" -> file.getName)
+          )
+        )
+      )
+
+    Post("/geocode", formData) ~> routes ~> check {
+      status mustBe OK
+      contentType.mediaType mustBe `text/csv`
+      val responseChunks = chunks
+      responseChunks.length mustBe 3
+      val results = responseChunks.map(c => c.data.decodeString("UTF-8"))
+      results(0).trim() mustBe "3146 M St NW Washington DC 20007,-77.06194807357616,38.90508593441382"
+      results(1).trim() mustBe "6774 Greers Ferry Rd Higden AR 72067,-92.14845079658727,35.5834810460401"
+      results(2).trim() mustBe "ADDRESS NOT FOUND,0.0,0.0"
+
+    }
+
+  }
+
 
 }
