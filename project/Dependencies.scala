7e2292eda1e9ce8beb1f516043fd016fdffdb130
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-11-05 20:09:56 -0500

    Add missing Resolver

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,8 @@ object Dependencies {
   val repos = Seq(
     "Local Maven Repo"  at "file://" + Path.userHome.absolutePath + "/.m2/repository",
     "Typesafe Repo"     at "http://repo.typesafe.com/typesafe/releases/",
-    "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases"
+    "Sonatype releases" at "https://oss.sonatype.org/content/repositories/releases",
+    Resolver.bintrayRepo("mfglabs", "maven")
   )
 
   val akkaActor          = "com.typesafe.akka"          %% "akka-actor"                           % Version.akka
