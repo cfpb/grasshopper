0a71456f4e14cb13588d4c9e66c4baa354d586a0
Juan Marin Otero <juan.marin.otero@gmail.com>
2015-12-10 10:23:35 -0500

    Project feature to EPSG:3857

--- geocoder/src/main/resources/webapp/js/metrics.js
+++ geocoder/src/main/resources/webapp/js/metrics.js
@@ -55,55 +55,107 @@ var map = new ol.Map({
   })
 });
 
-var editSource = new ol.source.Vector({
+var image = new ol.style.Circle({
+  radius: 5,
+  fill: null,
+  stroke: new ol.style.Stroke({color: 'red', width: 1})
+});
+
+var styles = {
+  'Point': [new ol.style.Style({
+    image: image
+  })],
+  'LineString': [new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'green',
+      width: 1
+    })
+  })],
+  'MultiLineString': [new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'green',
+      width: 1
+    })
+  })],
+  'MultiPoint': [new ol.style.Style({
+    image: image
+  })],
+  'MultiPolygon': [new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'yellow',
+      width: 1
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 0, 0.1)'
+    })
+  })],
+  'Polygon': [new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'blue',
+      lineDash: [4],
+      width: 3
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(0, 0, 255, 0.1)'
+    })
+  })],
+  'GeometryCollection': [new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'magenta',
+      width: 2
+    }),
+    fill: new ol.style.Fill({
+      color: 'magenta'
+    }),
+    image: new ol.style.Circle({
+      radius: 10,
+      fill: null,
+      stroke: new ol.style.Stroke({
+        color: 'magenta'
+      })
+    })
+  })],
+  'Circle': [new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'red',
+      width: 2
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(255,0,0,0.2)'
+    })
+  })]
+};
+
+var styleFunction = function(feature, resolution) {
+  return styles[feature.getGeometry().getType()];
+};
+
+var vectorSource = new ol.source.Vector({
   features: [],
-  format: new ol.format.GeoJSON()
+  projection: 'EPSG:3857'
 });
 
-var edit = new ol.layer.Vector({
-  source: editSource
+var vectorLayer = new ol.layer.Vector({
+  source: vectorSource,
+  style: styleFunction
 });
 
-map.addLayer(edit);
-
+map.addLayer(vectorLayer);
 
 function updateFeatures (msg) {
-  var features = msg.features;
-  var geoJsonObject = {
-    'type': 'FeatureCollection',
-    'features': []
-  }
-
-  var vectorSource = new ol.source.Vector({
-    features: (new ol.format.GeoJSON()).readFeatures(geoJsonObject)
-  });
-
-  for (i in features) {
-    var geoJson = features[i];
-    //var geomWGS84 = new ol.format.GeoJSON().readGeometry(geoJson);
-    //console.log(geomWGS84);
-    //var geom = geomWGS84.transform('EPSG:4326','EPSG:3857');
-    //console.log(geom);
-  }
-
-
+  vectorSource.clear(false);
+  var fc = msg.fc;
   var geoJsonFormat = new ol.format.GeoJSON({
     defaultProjection: 'EPSG:4326'
   });
-
-  var features = msg.features;
+  var features = geoJsonFormat.readFeatures(fc);
   for (i in features) {
-    var geoJson = features[i];
-    //console.log(geoJson);
-    //var geomWGS84 = geoJsonFormat.readGeometry(geoJson);
-    //var geom = geomWGS84.transform('EPSG:4326','EPSG:3857');
-    //var feature = new ol.Feature({
-    //  geometry: geom
-    //});
-    //if (editSource.getFeatures().length > maxFeatures) {
-    //  var lastFeature = editSource.getFeatures()[0];
-    //  editSource.removeFeature(lastFeature);
-    //}
-    //editSource.addFeature(feature);
+    var feature = features[i];
+    var coords = new ol.proj.transform(feature.getGeometry().getFirstCoordinate(), 'EPSG:4326','EPSG:3857');
+    var point = new ol.geom.Point(coords);
+    var f = new ol.Feature({
+      geometry: point
+    });
+    vectorSource.addFeature(f);
   }
 }
